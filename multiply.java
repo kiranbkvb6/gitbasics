02e732630fb29f3c3cb2df3a33cc1535e61ec11f
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-07-26 11:50:51 +0530

    added mukltiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+package GitBasics;
+
+public class multiply {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int product = a * b;
+        System.out.println(product);
+    }
+}
