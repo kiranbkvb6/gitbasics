67812cb24c12353b57e556eee77671901f28c493
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-07-26 11:39:04 +0530

    difference saved

---
+++ diff.java
@@ -0,0 +1,10 @@
+package GitBasics;
+
+public class diff {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = a - b;
+        System.out.println(difference);
+    }
+}
