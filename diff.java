0c1bfd827327aecfb598ac652cc334370ce0aa09
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-07-26 12:33:24 +0530

    changed the variable name

--- diff.java
+++ diff.java
@@ -4,7 +4,7 @@ public class diff {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        int difference = a - b;
-        System.out.println(difference);
+        int diffe = a - b;
+        System.out.println(diffe);
     }
 }
