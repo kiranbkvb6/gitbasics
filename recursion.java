daae07f21b69cab6b403b1b276cdc0e70abb4347
kiranbkvb6 <kiranbkvb6@gmail.com>
2025-07-27 08:56:54 +0530

    full recursion

---
+++ recursion.java
@@ -0,0 +1,17 @@
+package GitBasics;
+
+public class recursion {
+    public static void main(String[] args) {
+        int n = 5; // Example input
+        int result = factorial(n);
+        System.out.println("Factorial of " + n + " is: " + result);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1; // Base case
+        } else {
+            return n * factorial(n - 1); // Recursive case
+        }
+    }
+}
